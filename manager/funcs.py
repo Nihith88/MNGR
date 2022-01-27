ccf09f3ac40ffcedb588335ec35321e9986f12b6
Nihith88 <nihith88@yandex.ru>
2022-01-27 14:53:35 +0300

    Продолжаем работу на менеджером

--- manager/funcs.py
+++ manager/funcs.py
@@ -1,32 +1,57 @@
-import sys
+import main
 import os
+import shutil
 import datetime
 
-def ls_fldr():
-    return os.listdir(os.getcwd())
+def get_log(event=None):
+    cur_time = datetime.datetime.now()
+    result = f'{cur_time} -> {event}'
+    with open(main.logfilename, 'a', encoding='utf-8') as lf:
+        lf.write(result, '\n')
 
-def cr_fldr(usr_dir_name):
-    usr_dir_name = (input('Введите название новой папки') if str else 'Новая папка')
-    os.mkdir(os.path.join(os.getcwd(), usr_dir_name))
 
-def rm_fldr():
-    pass
+def cr_fldr():  # Создание папки с пользовательским именем
+    name = input('Введите название новой папки')
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        print('Такая папка уже существует')
+
+
+def rm_fldr():   # удаление папки с указанным именем
+    usr_dir_name = input('Введите название папки для удаления')
+    os.rmdir(usr_dir_name)
+
 
 def copy_fldr():
-    pass
+    name = input('Введите название папки для копирования')
+    new_name = input('Введите название новой папки')
+    try:
+        shutil.copytree(name, new_name)
+    except FileExistsError:
+        print('такая папка уже существует')
 
-def lvl_up():
-    pass
 
-def ch_dir():
-    pass
+def get_list():
+    print(os.listdir())
 
 
-def cr_file():
-    pass
+def cr_file(text=None):  # Создание файла с возможностью записи некоторого текста
+    filename = input('Введите название для нового файла с расширением')
+    with open(filename, 'w', encoding='utf-8') as f:
+        if text:
+            f.write(text)
+
 
 def rm_file():
-    pass
+    name = input('Введите название файла для удаления')
+    os.remove(name)
+
 
 def copy_file():
-    pass
+    name = input('Введите название файла для копирования')
+    new_name = input('Введите название нового файла')
+    try:
+        shutil.copy(name, new_name)
+    except FileExistsError:
+        print('Такой файл уже существует')
--- manager/main.py
+++ manager/main.py
@@ -1,43 +1,46 @@
-import os, datetime, time
-import manager.funcs
-print(70*'=')
-print(21*' ', 'КОНСОЛЬНЫЙ ФАЙЛОВЫЙ МЕНЕДЖЕР', 21*' ')
-print(12*' ', 'СОЗДАНИЕ КОПИРОВАНИЕ И УДАЛЕНИЕ ФАЙЛОВ И ПАПОК', 12*' ')
-print(20*' ', 'СОХРАНЕНИЕ СВОЕЙ РАБОТЫ В ФАЙЛ', 20*' ')
-print(70*'=', '\n')
+import os
+import manager
+# import Guessnumber
+
+print(70 * '=')
+print(21 * ' ', 'КОНСОЛЬНЫЙ ФАЙЛОВЫЙ МЕНЕДЖЕР', 21 * ' ')
+print(12 * ' ', 'СОЗДАНИЕ КОПИРОВАНИЕ И УДАЛЕНИЕ ФАЙЛОВ И ПАПОК', 12 * ' ')
+print(20 * ' ', 'СОХРАНЕНИЕ СВОЕЙ РАБОТЫ В ФАЙЛ', 20 * ' ')
+print(70 * '=', '\n')
 
 usrname = input('Введите имя пользователя для ведения отчета:  ')
-logfilename = ('log_' + usrname + '_' + '.log')
+
+logfilename = ('log_{}.log' .format(usrname))
 lf = open(logfilename, 'w')
-lf.write('Сессия начата в ' + str(time.ctime()))
-lf.close()
+manager.funcs.get_log(event='Начало сессии')
 choise = '0'
 while choise != 'x':
-      print(usrname, 'Вы находитесь в каталоге ', os.getcwd(), '\n \n')
-      print('1 - посмотреть содержимое папки, 2 - создать папку, 3 - удалить папку, 4 - создать копию папки \n'
-            '5 - на уровень вверх, 6 - перейти в папку по имени, 7 - создать файл, 8 - удалить файл, '
-            '9 - скопировать файл, p - сыграть в игру, x - выход \n')
-      choise = input()
-      if choise == '1':  #
-            print(str(os.listdir(os.getcwd())))
-      elif choise == '2':  #
-            manager.funcs.cr_fldr()
-      elif choise == '3':  #
-            manager.funcs.rm_fldr()
-      elif choise == '4':  #
-            manager.funcs.copy_fldr()
-      elif choise == '5':  #
-            manager.funcs.lvl_up()
-      elif choise == '6':  #
-            manager.funcs.ch_dir()
-      elif choise == '7':  #
-            manager.funcs.cr_file()
-      elif choise == '8':  #
-            manager.funcs.rm_file()
-      elif choise == '9':  #
-            manager.funcs.copy_file()
-      elif choise == 'p':  #
-            pass
-      else:  #
-            break
-
+    print(usrname, 'Вы находитесь в каталоге ', os.getcwd(), '\n \n')
+    print('1 - посмотреть содержимое папки, 2 - создать папку, 3 - удалить папку, 4 - создать копию папки \n'
+          '5 - на уровень вверх, 6 - перейти в папку по имени, 7 - создать файл, 8 - удалить файл, '
+          '9 - скопировать файл, p - сыграть в игру, x - выход \n')
+    choise = input()
+    if choise == '1':  #
+        manager.funcs.get_list()
+    elif choise == '2':  #
+        manager.funcs.cr_fldr()
+      # ('Создание каталога ')
+    elif choise == '3':  #
+        manager.funcs.rm_fldr()
+     # ('Удаление каталога ')
+    elif choise == '4':  #
+        manager.funcs.copy_fldr()
+     #('Копирование каталога ')
+    elif choise == '7':  #
+        manager.funcs.cr_file()
+    #  ('Создание файла ')
+    elif choise == '8':  #
+        manager.funcs.rm_file()
+    #  ('Удаление файла ')
+    elif choise == '9':  #
+        manager.funcs.copy_file()
+    elif choise == 'p':  #
+        pass
+      #  Guessnumber
+    else:  #
+        break
